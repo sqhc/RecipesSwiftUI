fb7d1ee6369a53cdff6ecae37587e6c1c899c0c6
shenqing <shenqin1@msu.edu>
2023-05-02 12:53:04 -0400

    Initial Commit

---
+++ RecipesSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipesSwiftUI
+//
+//  Created by 沈清昊 on 5/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipesSwiftUI/RecipesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesSwiftUIApp.swift
+//  RecipesSwiftUI
+//
+//  Created by 沈清昊 on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
